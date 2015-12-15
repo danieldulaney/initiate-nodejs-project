92988a0c4b5b7b3eb23b82a3d079f996277c7b7d
danieldulaney <dulaney.daniel@gmail.com>
2015-12-15 03:40:46 -0500

    WIP initialize

--- initialize.cmd
+++ initialize.cmd
@@ -1,3 +1,18 @@
 @ECHO off
-echo Initializing new project...
+IF [%1]==[] (
+	set /p projname="Enter project name: "
+) ELSE (
+	set projname=%1%
+)
+
+
+echo Initializing new project in folder %projname%...
+IF NOT EXIST %projname% (mkdir %projname%)
+cd %projname%
 xcopy %~dp0template\*
+
+echo Running npm install...
+npm install
+
+echo Finalizing...
+cd ..
